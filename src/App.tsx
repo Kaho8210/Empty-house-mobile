4b1e91a6e7fd667f57326d6fa9b59e08f9ab7b37
Kaho8210 <s1280204@u-aizu.ac.jp>
2023-02-26 16:13:24 +0900

    2nd modification

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-import { MantineProvider,Button } from '@mantine/core';
+/*import { Button } from '@mantine/core';*/
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom" 
+import Login from './components/Login';
+import Home from './components/Home';
+import ParticipantHome from './components/ParticipantHome';
 
 function App() {
-  return (            
-        <div className="App">          
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            <MantineProvider>
-            <Button>It work</Button>
-          </MantineProvider>
-          </header>          
-        </div>        
+  return (
+      <Router>         
+        <div className="App">      
+
+          <Routes>
+            <Route path="/" element={<Login />} />    /* Root is Login page */
+            <Route path="/home" element={<Home />} />
+            <Route path="/home/participantHome" element={<ParticipantHome />} />
+          </Routes>
+        </div>     
+      </Router>   
   );
 }
+
 
 export default App;
---
+++ src/components/ParticipantHome.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ParticipantHome = () => {
+  return (
+    <div>ParticipantHome</div>
+  )
+}
+
+export default ParticipantHome;
