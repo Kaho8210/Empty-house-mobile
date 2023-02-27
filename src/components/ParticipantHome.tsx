c771f4b729e395a78d041a732ee5298c5b011e92
Toru Otake <s1290048@u-aizu.ac.jp>
2023-02-27 13:28:35 +0900

    cssファイルにボタンの位置を調整するプログラムを記入

--- src/components/ParticipantHome.tsx
+++ src/components/ParticipantHome.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import {Button,Center} from '@mantine/core';
+import {Button} from '@mantine/core';
 import "./ParticipantHome.css"
 
 const ParticipantHome = () => {
   return (
     <div> 
       <div>
-    <Button color="violet" radius="md" size="lg" top = {200}>
+    <Button color="violet" radius="md" size="lg" top={311}>
     行事の参加予定
   </Button>
 </div>
 <div>
 
-  <Button color="violet" radius="md" size="lg" top = {300}>
+  <Button color="violet" radius="md" size="lg" top={474}>
       予約している行事
     </Button>
    
